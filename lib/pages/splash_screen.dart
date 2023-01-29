5b2e89def877f36acbaea413353279845f2b931c
Haf1dzsr <hsramadhan68@gmail.com>
2023-01-29 14:54:28 +0700

    update

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
+import 'dart:async';
+
+import 'package:penggajian/pages/auth/login.dart';
+import 'package:penggajian/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 4),
+      () {
+        Get.off(LoginPage());
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: whiteColor,
+      body: SafeArea(
+        child: Container(
+          color: whiteColor,
+          width: Get.width,
+          height: Get.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/logo_brr.png',
+                width: 350,
+                height: 175,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
